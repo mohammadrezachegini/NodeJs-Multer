2d36053724ab4e07e380ad119d937e5672c5fd71
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-03 10:35:46 -0800

    add format validator for multer

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express")
 const app = express()
-
+const {ErrorHandler,NotFoundError} = require("./util/errorHandler")
 const {uploadFile} = require("./middleware/multer")
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-// app.use("upl oads",express.static("uploads"))
+app.use(express.static("public"))
 
 app.post("/upload", uploadFile.single("image") ,(req,res,next) => {
     console.log(req.body);
     res.send(req.file)
 })
+
+app.use(NotFoundError)
+app.use(ErrorHandler)
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- middleware/multer.js
+++ middleware/multer.js
@@ -8,8 +8,17 @@ const storage = multer.diskStorage({
     },
     filename: function (req,file,cb){
         const ext = path.extname(file.originalname);
-        const fileName = Date.now() + ext
-        cb(null, fileName)
+        const mimetype = path.extname(file.mimetype);
+        console.log(mimetype);
+        const whiteLisstFormat = ['.png','.jpg','.jpeg']
+        const whiteLisstMimType = ['image/png','image/jpg','image/jpeg']
+        if(whiteLisstFormat.includes(ext)){
+            const fileName = Date.now() + ext
+            cb(null, fileName)
+        }else {
+            cb(new Error("the file is invalid"))
+        }
+        
     }
 });
 
